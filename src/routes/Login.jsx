a069d41d1128925643e70ec8b2ff372e64103091
이창한 <momo0170@naver.com>
2024-03-24 21:48:27 +0900

    feature: 로그인, 회원가입 기능 추가

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,17 +1,41 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
+  const [loginInfo, setLoginInfo] = useState({
+    id: '',
+    password: '',
+  });
   const navigate = useNavigate();
-  const moveToDiaryList = () => {
-    navigate('/diary');
+  const handleLogin = (e) => {
+    e.preventDefault();
+    axios({
+      method: 'POST',
+      url: `${process.env.REACT_APP_BASE_URL}/sign-in`,
+      data: {
+        ...loginInfo,
+      },
+    }).then((data) => {
+      console.log(data);
+      navigate('/');
+    });
   };
+  const onChange = (e) => {
+    setLoginInfo({ ...loginInfo, [e.target.name]: e.target.value });
+  };
+  console.log(loginInfo);
   return (
     <main>
-      <form>
-        <input type="text" />
-        <input type="password" />
-        <button onClick={moveToDiaryList}>로그인</button>
+      <form onSubmit={handleLogin}>
+        <input name="id" type="text" placeholder="아이디" onChange={onChange} />
+        <input
+          name="password"
+          type="password"
+          placeholder="패스워드"
+          onChange={onChange}
+        />
+        <button onClick={handleLogin}>로그인</button>
       </form>
     </main>
   );
--- src/routes/Signup.jsx
+++ src/routes/Signup.jsx
@@ -1,15 +1,61 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Signup() {
+  const navigate = useNavigate();
+  const [userInfo, setUserInfo] = useState({
+    id: '',
+    password: '',
+    nickname: '',
+    name: '',
+    url: '',
+  });
+  const onChange = (e) => {
+    setUserInfo({ ...userInfo, [e.target.name]: e.target.value });
+  };
+  const handleSignUp = () => {
+    axios({
+      method: 'POST',
+      url: `${process.env.REACT_APP_BASE_URL}/sign-up`,
+      data: {
+        ...userInfo,
+      },
+    }).then((data) => {
+      console.log(data);
+      alert('가입이 완료되었습니다!');
+      navigate('/');
+    });
+  };
+  const moveToWelcome = () => {
+    navigate('/');
+  };
+  console.log(userInfo);
   return (
     <main>
-      <input type="text" />
-      <input type="text" />
-      <input type="text" />
-      <input type="text" />
-      <input type="text" />
-      <button>취소</button>
-      <button>가입</button>
+      <div>
+        <div>아이디</div>
+        <input name="id" type="text" onChange={onChange} />
+      </div>
+      <div>
+        <div>비밀번호</div>
+        <input name="password" type="password" onChange={onChange} />
+      </div>
+      <div>
+        <div>이름</div>
+        <input name="name" type="text" onChange={onChange} />
+      </div>
+      <div>
+        <div>닉네임</div>
+        <input name="nickname" type="text" onChange={onChange} />
+      </div>
+      <div>
+        <div>사용자 프로필 URL</div>
+        <input name="url" type="url" onChange={onChange} />
+      </div>
+
+      <button onClick={moveToWelcome}>취소</button>
+      <button onClick={handleSignUp}>가입</button>
     </main>
   );
 }
