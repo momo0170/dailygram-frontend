d3d4ae39dee633c5743d908ce565d8a5b32039ca
이창한 <momo0170@naver.com>
2024-03-26 14:56:30 +0900

    fix: 사용자 정보가 없을 때 에러 수정
     - 사용자 정보가 null 값일 때 구조 분해 할당이 불가능해서 값을 렌더링하지 못한 현상 수정

--- src/components/DiaryList.jsx
+++ src/components/DiaryList.jsx
@@ -4,12 +4,7 @@ import styles from './css/DiaryList.module.scss';
 
 export default function DiaryList({ diaryList, isCardClick, setIsCardClick }) {
   console.log(diaryList);
-  useEffect(() => {
-    const data = localStorage.getItem('usr');
-    if (!data) {
-      console.log('로그인된 상태가 아닙니다.');
-    }
-  });
+
   return (
     <main className={styles.main}>
       <ul>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,7 +4,6 @@ import styles from './css/Profile.module.scss';
 
 export default function Profile() {
   const [user, setUser] = useOutletContext();
-  const { nickname, url } = user;
   const navigate = useNavigate();
   const handleLogout = () => {
     localStorage.clear();
@@ -14,16 +13,22 @@ export default function Profile() {
     const data = localStorage.getItem('user');
     setUser(JSON.parse(data));
   }, []);
-  console.log(user);
+
   return (
-    <aside className={styles.main}>
-      <div className={styles.info}>
-        <img src={url} alt="profile-img" className={styles.profileImg} />
-        <span className={styles.nickname}>{nickname}</span>
-      </div>
-      <button onClick={handleLogout} className={styles.logoutBtn}>
-        로그아웃
-      </button>
-    </aside>
+    <>
+      <aside className={styles.main}>
+        <div className={styles.info}>
+          <img
+            src={user?.url}
+            alt="profile-img"
+            className={styles.profileImg}
+          />
+          <span className={styles.nickname}>{user?.nickname}</span>
+        </div>
+        <button onClick={handleLogout} className={styles.logoutBtn}>
+          로그아웃
+        </button>
+      </aside>
+    </>
   );
 }
