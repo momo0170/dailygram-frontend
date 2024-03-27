a8cb8df23f6268f94c5bf885e4199f11ca1cb889
이창한 <momo0170@naver.com>
2024-03-27 14:05:45 +0900

    feature: 로그인 에러 시 메시리를 보여주는 컴포넌트 추가
      - 아이디, 비밀번호를 잘못 입력했을 경우 입력창 밑에 메시지를 보여줌

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -9,6 +9,8 @@ export default function Login() {
     id: '',
     password: '',
   });
+  const [messageOfId, setMessageOfId] = useState('');
+  const [messageOfPassword, setMessageOfPassword] = useState('');
   const navigate = useNavigate();
   const handleLogin = (e) => {
     e.preventDefault();
@@ -18,15 +20,35 @@ export default function Login() {
       data: {
         ...loginInfo,
       },
-    }).then((data) => {
-      localStorage.setItem('user', JSON.stringify(data.data.user[0]));
-      setIsLogin((isLogin) => !isLogin);
-      navigate('/diary');
-    });
+    })
+      .then((data) => {
+        localStorage.setItem('user', JSON.stringify(data.data.user[0]));
+        setIsLogin((isLogin) => !isLogin);
+        navigate('/diary');
+      })
+      .catch((error) => {
+        console.log(error);
+        const {
+          response: { data, status },
+        } = error;
+        // 발생한 에러가 401(Unauthorized)면
+        if (status === 401) {
+          if (data.message === '가입되지 않은 사용자입니다.') {
+            setMessageOfId(data.message);
+            setMessageOfPassword('');
+          }
+          if (data.message === '비밀번호가 일치하지 않습니다.') {
+            setMessageOfPassword(data.message);
+            setMessageOfId('');
+          }
+        }
+      });
   };
   const onChange = (e) => {
     setLoginInfo({ ...loginInfo, [e.target.name]: e.target.value });
   };
+  console.log(messageOfId);
+  console.log(messageOfPassword);
 
   return (
     <main className={styles.main}>
@@ -36,16 +58,45 @@ export default function Login() {
           type="text"
           placeholder="아이디"
           onChange={onChange}
-          className={styles.id}
+          className={styles.idInput}
           autoComplete="off"
+          style={
+            messageOfId
+              ? {
+                  outline: '2px solid red',
+                  border: 'none',
+                }
+              : {}
+          }
         />
+        <div
+          style={messageOfId ? { opacity: 1 } : { opacity: 0 }}
+          className={styles.idMessage}
+        >
+          {messageOfId}
+        </div>
+
         <input
           name="password"
           type="password"
           placeholder="패스워드"
           onChange={onChange}
-          className={styles.password}
+          className={styles.passwordInput}
+          style={
+            messageOfPassword
+              ? {
+                  outline: '2px solid red',
+                  border: 'none',
+                }
+              : {}
+          }
         />
+        <div
+          style={messageOfPassword ? { opacity: 1 } : { opacity: 0 }}
+          className={styles.passwordMessage}
+        >
+          {messageOfPassword}
+        </div>
         <button onClick={handleLogin} className={styles.loginBtn}>
           로그인
         </button>
