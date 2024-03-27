4b19ad493da9f962f72a9146b7a64302685077fd
이창한 <momo0170@naver.com>
2024-03-27 19:58:03 +0900

    design: <Profile> 컴포넌트 디자인
     - 화살표 클릭 시 상세 메뉴 보이도록 수정

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,14 @@
-import React, { useEffect } from 'react';
-import { useNavigate, useOutletContext } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useOutletContext } from 'react-router-dom';
 import styles from './css/Profile.module.scss';
+import { IoIosArrowDown } from 'react-icons/io';
+import { BsBrightnessHigh } from 'react-icons/bs';
+import { BsFillMoonFill } from 'react-icons/bs';
 
 export default function Profile() {
   const [user, setUser, isLogin, setIsLogin] = useOutletContext();
+  const [isClick, setIsClick] = useState(false);
+
   const handleLogout = () => {
     localStorage.clear();
     setIsLogin((isLogin) => !isLogin);
@@ -16,17 +21,32 @@ export default function Profile() {
   return (
     <>
       <aside className={styles.main}>
-        <div className={styles.info}>
-          <img
-            src={user?.url}
-            alt="profile-img"
-            className={styles.profileImg}
-          />
-          <span className={styles.nickname}>{user?.nickname}</span>
-        </div>
-        <button onClick={handleLogout} className={styles.logoutBtn}>
-          로그아웃
-        </button>
+        <section className={styles.col}>
+          <div className={styles.info}>
+            <img
+              src={user?.url}
+              alt="profile-img"
+              className={styles.profileImg}
+            />
+            <span className={styles.nickname}>{user?.nickname}</span>
+          </div>
+          <div className={styles.btnAndIcon}>
+            <button onClick={handleLogout} className={styles.logoutBtn}>
+              로그아웃
+            </button>
+            <IoIosArrowDown
+              size="20"
+              className={`${styles.arrow} ${isClick ? styles.clicked : ''} `}
+              onClick={() => setIsClick((isClick) => !isClick)}
+            />
+          </div>
+        </section>
+        <section
+          className={`${styles.darkMode} ${isClick ? styles.clicked : ''}`}
+        >
+          <BsBrightnessHigh size="25" />
+          <span className={styles.text}>다크모드</span>
+        </section>
       </aside>
     </>
   );
